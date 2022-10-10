3eecf8772a27b06ceac38474e7fee5f0c9ff91dc
Andrew Cavallo <65136929+Awire9966@users.noreply.github.com>
2022-10-10 07:11:34 -0400

    Add files via upload

---
+++ AddToPath/AddToPath/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddToPath
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var currval = Environment.GetEnvironmentVariable("Path", EnvironmentVariableTarget.Machine);
+            Environment.SetEnvironmentVariable("Path", currval + ";" + args.ToString(), EnvironmentVariableTarget.Machine);
+        }
+    }
+}
